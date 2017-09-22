4682e0394d9fa64ae619285b6c8f6c1076a08bc2
Keith Warren <contact@drixevel.com>
2017-09-22 19:23:11 -0400

    updated compile bat
    what title says

--- scripting/compile.bat
+++ scripting/compile.bat
@@ -1,5 +1,5 @@
-spcomp zones_manager.sp	-o../plugins/zones_manager.smx
-spcomp zones_manager.sp -o E:\Servers\TF2\tf\addons\sourcemod\plugins\zones_manager.smx
+spcomp zones_manager.sp			-o../plugins/zones_manager.smx
+spcomp arena_zm.sp			-o../plugins/arena_zm.smx
 spcomp zones_effect_testzones.sp	-o../plugins/zones_effect_testzones.smx
-spcomp zones_effect_testzones.sp -o E:\Servers\TF2\tf\addons\sourcemod\plugins\zones_effect_testzones.smx
+spcomp zones_effect_meleeonly.sp	-o../plugins/zones_effect_meleeonly.smx
 pause
